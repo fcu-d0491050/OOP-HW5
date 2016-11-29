9e1a12029a12ecbf0af21e6b80c53d1845129e16
ChaiChingTing <joliangel288@gmail.com>
2016-11-30 03:19:09 +0800

    Main JAVA

---
+++ src/fcu/iecs/oop/pokemon/Main.java
@@ -0,0 +1,13 @@
+package fcu.iecs.oop.pokemon;
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		  Pokemon p1 = new Pokemon(“Psyduck”, 100);
+		  Pokemon p2 = new Pokemon(“Pikachu”, 300);
+		  GYM.fight(p1, p2);
+	}
+
+
+}
