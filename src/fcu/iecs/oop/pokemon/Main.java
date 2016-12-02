1b7544118543c9fb5a92d3620ac48cbd541703b0
fcu-d0491050 <joliangel288@gmail.com>
2016-12-03 00:56:08 +0800

    Update Main.java

--- src/fcu/iecs/oop/pokemon/Main.java
+++ src/fcu/iecs/oop/pokemon/Main.java
@@ -4,8 +4,8 @@ public class Main {
 	
 	public static void main(String[] args)
 	{
-		  Pokemon p1 = new Pokemon(“Psyduck”, 100);
-		  Pokemon p2 = new Pokemon(“Pikachu”, 300);
+		  Pokemon p1 = new Pokemon("Psyduck”, 100);
+		  Pokemon p2 = new Pokemon("Pikachu", 300);
 		  GYM.fight(p1, p2);
 	}
 
