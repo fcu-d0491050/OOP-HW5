6b12de5b87ab4632a6d0e62eb8b8eb89a2fde33d
fcu-d0491050 <joliangel288@gmail.com>
2016-12-03 01:05:59 +0800

    Update GYM.java

--- src/fcu/iecs/oop/pokemon/GYM.java
+++ src/fcu/iecs/oop/pokemon/GYM.java
@@ -5,9 +5,18 @@ public class GYM
 
 	public static void fight(Pokemon p1, Pokemon p2)
 	{
-		System.out.printf("Winner is %s", Pokemon.getName());
-		Pokemon.increaseCP();
+		if((int)(Math.random()*2+1)==1)
+		{
+			System.out.println("Winner is "+p1.getName());
+			p1.setCp(100);
+			System.out.println(p1.getCp()+500); 
+		}
+		else
+		{
+			System.out.println("Winner is "+p2.getName());
+			p2.setCp(300);
+			System.out.println(p2.getCp()+500);
+		}
 	}
-
 	
 }
